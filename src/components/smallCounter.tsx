c3e96ce8897da760cd255afcccc0f81e6184ae7b
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-12 16:40:40 +0100

    Add new useState, deep copy

--- src/components/smallCounter.tsx
+++ src/components/smallCounter.tsx
@@ -5,58 +5,66 @@ import {Button} from "./Button";
 
 export const SmallCounter = () => {
 
-    const [value, setValue] = useState(0)
-    const [maxValue, setMaxValue] = useState(0)
-    const [startValue, setStartValue] = useState(0)
+    const [value, setValue] = useState({
+        maxValue: 0,
+        startValue: 0,
+        counterValue: 0,
+    })
+
 
     const onIncHandler = () => {
-        setValue(value + 1)
+        setValue({...value, counterValue: value.counterValue + 1})
     }
 
     const onResetHandler = () => {
-        setValue(0)
+        setValue({...value, counterValue: value.startValue})
     }
 
     const setButtonHandler = () => {
-
+        setValue({...value, counterValue: value.startValue})
+        console.log(value)
     }
+
     const maxValueonChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(e.currentTarget.value, 10);
-        setMaxValue(value);
+        const valueMax = parseInt(e.currentTarget.value, 10);
+        console.log(e)
+        setValue({...value, maxValue: valueMax})
+        console.log()
     }
     const startValueonChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(e.currentTarget.value, 10);
-        setStartValue(value);
+        const valueStart = parseInt(e.currentTarget.value, 10);
+        setValue({...value, startValue: valueStart})
     }
+
+    const isDisabled = value.maxValue === value.counterValue
     return (
         <div className={s.app}>
             <div className={s.conteiner}>
                 <div className={s.indecator}>
-                <span className={s.input} onChange={maxValueonChangeHandler}> max value <input type="number"/> </span>
-                    <div className={s.input} onChange={startValueonChangeHandler}>start value <input type="number"/></div>
-                    <Button name={"set"} callback={() => {}}/>
-                </div>
-            </div>
-
-                <div className={s.container}>
-
-                    <div className={s.indecator + (value >= 5 ? " " + s.red : "")}>
-                        <div className={s.input}> {value}</div>
-                        <div>
-
-                            <Button name={'inc'} callback={() => {
-                                onIncHandler()
-                            }} disabled={value >= 5}/>
-                            <Button name={'reset'} callback={() => {
-                                onResetHandler()
-                            }}/>
-
-                        </div>
+                    <span className={s.input} > max value <input
+                        type="number" value={value.maxValue} onChange={maxValueonChangeHandler}/> </span>
+                    <div className={s.input} >start value <input type="number" value={value.startValue} onChange={startValueonChangeHandler}/>
                     </div>
+                    <Button name={"set"} callback={setButtonHandler}/>
+                </div>
+            </div>
 
+            <div className={s.container}>
+
+                {/*<div className={s.indecator + (value.counterValue >= 5 ? " " + s.red : "")}>*/}
+                <div className={s.indecator + (isDisabled ? " " + s.red : "")}>
+                    <div className={s.input}> {value.counterValue}</div>
+                    <div>
+
+                        <Button name={'inc'} callback={onIncHandler} disabled={isDisabled}/>
+                        <Button name={'reset'} callback={onResetHandler}/>
+
+                    </div>
                 </div>
 
             </div>
-            );
-            }
+
+        </div>
+    );
+}
 
