f9096baddde0437844154886c6d506c0ff11b493
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-26 23:47:38 +0200

    Add local storage

--- src/components/smallCounter.tsx
+++ src/components/smallCounter.tsx
@@ -2,18 +2,25 @@ import React, {ChangeEvent, useState} from 'react';
 import s from './App.module.css'
 import {Button} from "./Button";
 
+type UseStateType={
+    maxValue: number
+    startValue: number
+    counterValue: number
+
+}
 
 export const SmallCounter = () => {
 
-    const [value, setValue] = useState({
-        maxValue: 0,
-        startValue: 0,
-        counterValue: 0,
-    })
+    const [value, setValue] = useState<UseStateType>({
+        maxValue: Number(localStorage.getItem('maxValue')),
+        startValue:  Number(localStorage.getItem('startValue')),
+        counterValue: Number(localStorage.getItem('counterValue')),
 
+    })
 
     const onIncHandler = () => {
         setValue({...value, counterValue: value.counterValue + 1})
+        localStorage.setItem('counterValue',String(value.counterValue + 1))
     }
 
     const onResetHandler = () => {
@@ -22,36 +29,41 @@ export const SmallCounter = () => {
 
     const setButtonHandler = () => {
         setValue({...value, counterValue: value.startValue})
-        console.log(value)
+        localStorage.setItem('counterValue',String(value.startValue))
     }
 
     const maxValueonChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         const valueMax = parseInt(e.currentTarget.value, 10);
-        console.log(e)
         setValue({...value, maxValue: valueMax})
-        console.log()
+        localStorage.setItem('maxValue', e.currentTarget.value)
+
     }
     const startValueonChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         const valueStart = parseInt(e.currentTarget.value, 10);
         setValue({...value, startValue: valueStart})
+        localStorage.setItem('startValue', e.currentTarget.value)
     }
+    const isDisabled = value.maxValue ===  value.counterValue
 
-    const isDisabled = value.maxValue === value.counterValue
+
+
     return (
         <div className={s.app}>
             <div className={s.conteiner}>
                 <div className={s.indecator}>
                     <span className={s.input} > max value <input
-                        type="number" value={value.maxValue} onChange={maxValueonChangeHandler}/> </span>
-                    <div className={s.input} >start value <input type="number" value={value.startValue} onChange={startValueonChangeHandler}/>
+                        onChange={maxValueonChangeHandler}
+                        value={value.maxValue}
+                        type="number"/> </span>
+                    <div className={s.input} >start value <input value={value.startValue}
+                                                                 onChange={startValueonChangeHandler}
+                                                                 type="number"/>
                     </div>
                     <Button name={"set"} callback={setButtonHandler}/>
                 </div>
             </div>
 
             <div className={s.container}>
-
-                {/*<div className={s.indecator + (value.counterValue >= 5 ? " " + s.red : "")}>*/}
                 <div className={s.indecator + (isDisabled ? " " + s.red : "")}>
                     <div className={s.input}> {value.counterValue}</div>
                     <div>
@@ -67,4 +79,3 @@ export const SmallCounter = () => {
         </div>
     );
 }
-
